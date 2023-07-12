9f0b56505e37a6a000e2c1dac5092ee919319c6d
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-07-13 03:46:45 +0700

    Dart basic

--- dart_basic/bin/dart_basic.dart
+++ dart_basic/bin/dart_basic.dart
@@ -1,7 +1,19 @@
-import 'package:dart_basic/dart_basic.dart' as dart_basic;
+void main() {
+  var firstName = "Gustio";
+  var lastName = "Nusamba";
+  print("My name is $firstName $lastName");
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_basic.calculate()}!');
+  print("\nSimple calculator");
+  int num1 = 20;
+  int num2 = 5;
+
+  var sum = num1 + num2;
+  var diff = num1 - num2;
+  var mul = num1 * num2;
+  double div = num1 / num2;
+
+  print("The sum is $sum");
+  print("The difference is $diff");
+  print("The multiplication is $mul");
+  print("The division is $div");
 }
-
-
