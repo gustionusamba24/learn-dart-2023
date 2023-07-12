8b0bdbb69ffc931006478f0b4dfcbe4bae8b45b3
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-07-13 04:13:29 +0700

    Variables in dart

---
+++ dart_basic/bin/dart_variables.dart
@@ -0,0 +1,17 @@
+void main() {
+  String name = "Agus Setianto";
+  String address = "Juwangen";
+  num age = 20;
+  num height = 1.7;
+  bool isMarried = false;
+
+  print("Name is $name");
+  print("Address is $address");
+  print("Age is $age");
+  print("Height is $height");
+  print("Married Status is $isMarried");
+
+  const pi = 3.14;
+  // pi = 4.23; // not possible
+  print("Value of PI is $pi");
+}
